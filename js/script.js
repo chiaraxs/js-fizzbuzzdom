758a11be692fda2d56342dca989075ed29c35666
chiaraxs <chiaracardinale92@gmail.com>
2021-12-11 16:26:19 +0100

    console output added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100.
 
+// 1. FISSO DICHIARAZIONE VARIABILE TRA JS E HTML
+let boxes = document.querySelector('.container-box');
+
+
+// 2. CREO COSTANTE X
+let x = 1;
+
+// 3. CREO CONTATORE CHE INCREMENTA FINCHE' NON RAGGIUNGE 100 (LIMITE PREDEFINITO)
+// 4. STAMPO IN CONSOLE I NUMERI DA 1 A 100
+
+for (let x = 1; x <= 100; x++) {
+    console.log(x);
+}
+
+
+
+
+
 // MILESTONE 1
 // Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
